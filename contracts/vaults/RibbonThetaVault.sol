527ecb1a4fbc4b40b5538d85216ea09c45915761
kenchangh <guanhao3797@gmail.com>
2021-06-06 00:39:31 +0100

    Working deposit tests

--- contracts/vaults/RibbonThetaVault.sol
+++ contracts/vaults/RibbonThetaVault.sol
@@ -277,8 +277,9 @@ contract RibbonThetaVault is DSMath, OptionsVaultStorage {
         require(asset == WETH, "Not WETH");
         require(msg.value > 0, "No value");
 
-        IWETH(WETH).deposit{value: msg.value}();
         _deposit(msg.value);
+
+        IWETH(WETH).deposit{value: msg.value}();
     }
 
     /**
@@ -286,8 +287,9 @@ contract RibbonThetaVault is DSMath, OptionsVaultStorage {
      * @param amount is the amount of `asset` to deposit
      */
     function deposit(uint256 amount) external nonReentrant {
-        IERC20(asset).safeTransferFrom(msg.sender, address(this), amount);
         _deposit(amount);
+
+        IERC20(asset).safeTransferFrom(msg.sender, address(this), amount);
     }
 
     /**
@@ -296,7 +298,7 @@ contract RibbonThetaVault is DSMath, OptionsVaultStorage {
      */
     function _deposit(uint256 amount) private {
         uint16 _round = round;
-        uint256 totalWithDepositedAmount = totalBalance();
+        uint256 totalWithDepositedAmount = totalBalance().add(amount);
 
         require(amount < MAX_UINT128, "Overflow");
         require(totalWithDepositedAmount < cap, "Exceed cap");
