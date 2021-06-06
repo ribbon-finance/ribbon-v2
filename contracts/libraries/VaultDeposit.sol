2e44542bf9510ae32ba70eac95aec9f43a320220
kenchangh <guanhao3797@gmail.com>
2021-06-06 03:49:04 +0100

    Guard and comments

--- contracts/libraries/VaultDeposit.sol
+++ contracts/libraries/VaultDeposit.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.7.3;
 
 library VaultDeposit {
-    struct DepositReceipt {
+    struct PendingDeposit {
+        // Flag to mark if processed or not
         bool processed;
+        // Maximum of 65535 rounds. Assuming 1 round is 7 days, maximum is 1256 years.
         uint16 round;
+        // Deposit amount
         uint128 amount;
     }
 }
