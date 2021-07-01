6a43f982fee45660e817ef9c357336a80b003ee6
John Doe <j@j>
2021-07-01 10:02:48 -0400

    remove sleep() from utils

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -274,7 +274,3 @@ export function decodeOrder(bytes: string): Order {
     buyAmount: BigNumber.from("0x" + bytes.substring(19, 42)),
   };
 }
-
-export function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
