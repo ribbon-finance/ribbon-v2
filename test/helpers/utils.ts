c5dd63e46560bc5a6712423e681472bd5c8de2e3
Chuddy <jacob4seinfeld@gmail.com>
2021-07-15 19:20:42 +0200

    add claimFromParticipantOrder (#42)
    Co-authored-by: John Doe <j@j>

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -274,3 +274,12 @@ export function decodeOrder(bytes: string): Order {
     buyAmount: BigNumber.from("0x" + bytes.substring(19, 42)),
   };
 }
+
+export function encodeOrder(order: Order): string {
+  return (
+    "0x" +
+    order.userId.toHexString().slice(2).padStart(16, "0") +
+    order.buyAmount.toHexString().slice(2).padStart(24, "0") +
+    order.sellAmount.toHexString().slice(2).padStart(24, "0")
+  );
+}
