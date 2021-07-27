39e22afa94beb542e042bed49e4119e236a349d2
kenchangh <guanhao3797@gmail.com>
2021-07-27 13:19:27 +0100

    Fix GnosisAuction conflict

---
+++ contracts/interfaces/IRibbonThetaVault.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+pragma experimental ABIEncoderV2;
+
+library Vault {
+    struct VaultParams {
+        // Option type the vault is selling
+        bool isPut;
+        // Token decimals for vault shares
+        uint8 decimals;
+        // Asset used in Theta / Delta Vault
+        address asset;
+        // Underlying asset of the options sold by vault
+        address underlying;
+        // Minimum supply of the vault shares issued, for ETH it's 10**10
+        uint56 minimumSupply;
+        // Vault cap
+        uint104 cap;
+        // Round 1 asset/share price
+        uint256 initialSharePrice;
+    }
+
+    struct OptionState {
+        // Option that the vault is shorting / longing in the next cycle
+        address nextOption;
+        // Option that the vault is currently shorting / longing
+        address currentOption;
+        // The timestamp when the `nextOption` can be used by the vault
+        uint32 nextOptionReadyAt;
+    }
+
+    struct VaultState {
+        // 32 byte slot 1
+        //  Current round number. `round` represents the number of `period`s elapsed.
+        uint16 round;
+        // Amount that is currently locked for selling options
+        uint104 lockedAmount;
+        // Amount that was locked for selling options in the previous round
+        // used for calculating performance fee deduction
+        uint104 lastLockedAmount;
+        // 32 byte slot 2
+        // Stores the total tally of how much of collateral there is
+        // to be used to mint rTHETA tokens
+        uint128 totalPending;
+        // Amount locked for scheduled withdrawals;
+        uint128 queuedWithdrawShares;
+    }
+}
+
+interface IRibbonThetaVault {
+    function currentOption() external view returns (address _currentOption);
+
+    function nextOption() external view returns (address _nextOption);
+
+    function vaultParams() external view returns (Vault.VaultParams memory);
+
+    function vaultState() external view returns (Vault.VaultState memory);
+
+    function optionState() external view returns (Vault.OptionState memory);
+
+    function optionAuctionID() external view returns (uint256 _auctionID);
+}
