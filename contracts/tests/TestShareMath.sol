a36adf176eacc4459d3aeddba9725aac0c328462
Ken Chan <guanhao3797@gmail.com>
2021-07-29 14:47:16 +0100

    Ken/chainsafe fix (#54)
    * Simplify mint calculation
    
    * Fix redundant check
    
    * Fix lint
    
    * Allow initiateWithdraw to be done when there's deposit
    
    * Fix compile
    
    * Test sharemath
    
    * Format lint
    
    * Fix tests

---
+++ contracts/tests/TestShareMath.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+import {ShareMath} from "../libraries/ShareMath.sol";
+
+contract TestShareMath {
+    function underlyingToShares(
+        uint256 underlyingAmount,
+        uint256 pps,
+        uint8 decimals
+    ) external pure returns (uint104) {
+        return ShareMath.underlyingToShares(underlyingAmount, pps, decimals);
+    }
+
+    function sharesToUnderlying(
+        uint256 shares,
+        uint256 pps,
+        uint8 decimals
+    ) external pure returns (uint256) {
+        return ShareMath.sharesToUnderlying(shares, pps, decimals);
+    }
+}
