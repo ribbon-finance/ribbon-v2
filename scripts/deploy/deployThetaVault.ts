e6d8bb6217445dd1f34084486edaae24b47c0a3a
kenchangh <guanhao3797@gmail.com>
2021-08-06 17:41:14 +0100

    Working StrikeSelection deploy

--- scripts/deploy/deployThetaVault.ts
+++ scripts/deploy/deployThetaVault.ts
@@ -18,13 +18,24 @@ require("dotenv").config();
 const ethusdcPool = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8";
 const wbtcusdcPool = "0x99ac8ca7087fa4a2a1fb6357269965a2014abc35";
 
-const wethPriceOracleAddress = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419";
-const wbtcPriceOracleAddress = "0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c";
-const usdcPriceOracleAddress = "0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6";
-
 const steps = {
   [WETH_ADDRESS]: 100,
   [WBTC_ADDRESS]: 1000,
+};
+
+const oracles = {
+  wbtc: {
+    mainnet: "0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c",
+    rinkeby: "0xECe365B379E1dD183B20fc5f022230C044d51404",
+  },
+  weth: {
+    mainnet: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+    rinkeby: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+  },
+  usdc: {
+    mainnet: "0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6",
+    rinkeby: "0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB",
+  },
 };
 
 const deployThetaVault = async (
@@ -34,8 +45,9 @@ const deployThetaVault = async (
   hre: HardhatRuntimeEnvironment
 ) => {
   const underlying = args.underlying;
+  const network = hre.network.name;
 
-  console.log("Network", hre.network.name);
+  console.log("Network", network);
 
   const signer = getDefaultSigner(
     "m/44'/60'/0'/0/0",
@@ -62,14 +74,19 @@ const deployThetaVault = async (
 
   const volOracle = await ManualVolOracle.deploy(signer.address);
 
+  // const volOracle = "0xDbF77e2D8d874224f4567310A8D97151e6CB25c9";
+
+  const underlyingOracle =
+    oracles[underlying === WETH_ADDRESS ? "weth" : "wbtc"][network];
+  const pool = underlying === WETH_ADDRESS ? ethusdcPool : wbtcusdcPool;
+
   const optionsPremiumPricer = await OptionsPremiumPricer.deploy(
-    underlying === WETH_ADDRESS ? ethusdcPool : wbtcusdcPool,
+    pool,
     volOracle.address,
-    underlying === WETH_ADDRESS
-      ? wethPriceOracleAddress
-      : wbtcPriceOracleAddress,
-    usdcPriceOracleAddress
+    underlyingOracle,
+    oracles.usdc[network]
   );
+  // const optionsPremiumPricer = "0xE8e1Df4F53c74853A2591222f557e655D484aDF2";
 
   if (!steps[underlying]) {
     throw new Error("No step set ");
@@ -83,8 +100,8 @@ const deployThetaVault = async (
     strikeStep
   );
 
-  const VaultLifecycle = await hre.ethers.getContractFactory("VaultLifecycle");
-  const vaultLifecycleLib = await VaultLifecycle.deploy();
+  // const VaultLifecycle = await hre.ethers.getContractFactory("VaultLifecycle");
+  // const vaultLifecycleLib = await VaultLifecycle.deploy();
 };
 
 export default deployThetaVault;
---
+++ scripts/helpers/getOrDeployContract.ts
@@ -0,0 +1,3 @@
+const getOrDeployContract = async () => {};
+
+export default getOrDeployContract;
