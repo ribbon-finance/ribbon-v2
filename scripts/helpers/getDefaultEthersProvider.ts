4113863fa78fa157d03e3f5c6b5e77511efcbedb
kenchangh <guanhao3797@gmail.com>
2021-08-10 00:18:43 +0100

    Remove old files

--- scripts/helpers/getDefaultEthersProvider.ts
+++ scripts/helpers/getDefaultEthersProvider.ts
@@ -2,52 +2,26 @@ import { ethers } from "ethers";
 
 require("dotenv").config();
 
-export type Networks = "mainnet" | "kovan" | "rinkeby";
+export type Networks = "mainnet" | "kovan";
 
 export const getDefaultProvider = (network: Networks = "kovan") => {
-  let url: string;
-
-  switch (network) {
-    case "mainnet":
-      url = process.env.MAINNET_URI;
-      break;
-    case "kovan":
-      url = process.env.KOVAN_URI;
-      break;
-    case "rinkeby":
-      url = process.env.RINKEBY_URI;
-      break;
-  }
+  const url =
+    network === "mainnet"
+      ? process.env.MAINNET_URI
+      : process.env.INFURA_KOVAN_URI;
 
   const provider = new ethers.providers.JsonRpcProvider(url);
 
   return provider;
 };
 
-export const getDefaultSigner = ({
-  path = "m/44'/60'/0'/0/0",
-  network = "kovan",
-}: {
-  path?: string;
-  network?: Networks;
-}) => {
-  let mnemonic: string;
-
-  switch (network) {
-    case "mainnet":
-      mnemonic = process.env.MNEMONIC;
-      break;
-    case "kovan":
-      mnemonic = process.env.KOVAN_MNEMONIC;
-      break;
-    case "rinkeby":
-      mnemonic = process.env.RINKEBY_MNEMONIC;
-      break;
-  }
+export const getDefaultSigner = (path: string, network: Networks = "kovan") => {
+  const mnemonic =
+    network === "mainnet" ? process.env.MNEMONIC : process.env.KOVAN_MNEMONIC;
 
   if (!mnemonic) {
     throw new Error("No mnemonic set");
   }
   const signer = ethers.Wallet.fromMnemonic(mnemonic, path);
-  return signer.connect(getDefaultProvider(network));
+  return signer;
 };
