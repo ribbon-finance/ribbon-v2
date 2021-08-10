57dd8d80de0432dd205f54b5f4d4836992e2db51
kenchangh <guanhao3797@gmail.com>
2021-08-10 23:08:22 +0100

    Initialize and commitAndClose tests

--- test/e2e/RibbonThetaVault-E2E.ts
+++ test/e2e/RibbonThetaVault-E2E.ts
@@ -1,14 +1,22 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import { parseEther } from "@ethersproject/units";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
+import { expect } from "chai";
 import { Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
 import hre from "hardhat";
+import { KOVAN_WETH, WETH_ADDRESS } from "../../constants/constants";
 import { assert } from "../helpers/assertions";
 
 const { deployments, ethers } = hre;
 
 let user: string;
 let userSigner: SignerWithAddress;
+let owner: string;
+let ownerSigner: SignerWithAddress;
+let feeRecipient: string;
+
+const weth = hre.network.name === "mainnet" ? WETH_ADDRESS : KOVAN_WETH;
 
 describe("E2E-RibbonThetaVault", () => {
   behavesLikeRibbonThetaVault({
@@ -17,9 +25,16 @@ describe("E2E-RibbonThetaVault", () => {
   });
 
   before(async () => {
-    const { deployer } = await hre.getNamedAccounts();
+    const {
+      deployer,
+      owner: _owner,
+      feeRecipient: _feeRecipient,
+    } = await hre.getNamedAccounts();
+    owner = _owner;
+    feeRecipient = _feeRecipient;
     user = deployer;
     userSigner = await ethers.getSigner(user);
+    ownerSigner = await ethers.getSigner(owner);
   });
 
   function behavesLikeRibbonThetaVault({
@@ -31,6 +46,10 @@ describe("E2E-RibbonThetaVault", () => {
   }) {
     describe(deploymentName, () => {
       let vault: Contract;
+      let assetContract: Contract;
+      let underlying: string;
+      let pricer: string;
+      let strikeSelection: string;
 
       before(async () => {
         const { address } = await deployments.get(deploymentName);
@@ -39,16 +58,39 @@ describe("E2E-RibbonThetaVault", () => {
           address,
           userSigner
         );
+        const { asset, underlying: _underlying } = await vault.vaultParams();
+        underlying = _underlying;
+        assetContract = await ethers.getContractAt("IERC20", asset, userSigner);
+        pricer = await vault.optionsPremiumPricer();
+        strikeSelection = await vault.strikeSelection();
+
+        const { abi: pricerABI } = await deployments.get(
+          "OptionsPremiumPricerETH"
+        );
+
+        const pricerContract = await ethers.getContractAt(
+          pricerABI,
+          pricer,
+          ownerSigner
+        );
+        const poolAddress = await pricerContract.pool();
+
+        const { address: volOracleAddress, abi: volOracleABI } =
+          await deployments.get("ManualVolOracle");
+        const volOracle = await ethers.getContractAt(
+          volOracleABI,
+          volOracleAddress,
+          ownerSigner
+        );
+
+        const annualizedVol = await volOracle.annualizedVol(poolAddress);
+        if (annualizedVol.isZero()) {
+          const setTx = await volOracle.setAnnualizedVol(parseUnits("1", 8));
+          await setTx.wait();
+        }
       });
 
       const depositIntoVault = async (vault: Contract) => {
-        const { asset } = await vault.vaultParams();
-        const assetContract = await ethers.getContractAt(
-          "IERC20",
-          asset,
-          userSigner
-        );
-
         const beforeVaultBalance = await assetContract.balanceOf(vault.address);
         const beforeShares = await vault.balanceOf(user);
 
@@ -88,15 +130,73 @@ describe("E2E-RibbonThetaVault", () => {
           await depositIntoVault(vault);
 
           const { amount: beforeAmount } = await vault.depositReceipts(user);
+          let beforeBalance: BigNumber;
+          if (assetContract.address === weth) {
+            beforeBalance = await ethers.provider.getBalance(user);
+          } else {
+            beforeBalance = await assetContract.balanceOf(user);
+          }
 
-          const withdrawTx = await vault.withdrawInstantly(depositAmount);
-          await withdrawTx.wait();
+          const gasPrice = parseUnits("1", "gwei");
+          const withdrawTx = await vault.withdrawInstantly(depositAmount, {
+            gasPrice,
+          });
+          const receipt = await withdrawTx.wait();
 
           const { round, amount, unredeemedShares } =
             await vault.depositReceipts(user);
           assert.equal(round, 1);
           assert.bnEqual(amount, beforeAmount.sub(depositAmount));
           assert.equal(unredeemedShares, 0);
+
+          if (assetContract.address === weth) {
+            const gasFee = gasPrice.mul(receipt.gasUsed);
+            assert.bnEqual(
+              await ethers.provider.getBalance(user),
+              beforeBalance.add(depositAmount).sub(gasFee)
+            );
+          } else {
+            assert.bnEqual(
+              await assetContract.balanceOf(user),
+              beforeBalance.add(depositAmount)
+            );
+          }
+        });
+      });
+
+      describe("initialize", () => {
+        it("cannot call initialize", async () => {
+          const args = [
+            owner,
+            feeRecipient,
+            0,
+            0,
+            "Ribbon ETH Theta Vault",
+            "rETH-THETA",
+            pricer,
+            strikeSelection,
+            50, //5% discount
+            3600, // 1 hour auction duration
+            {
+              isPut: false,
+              decimals: 18,
+              asset: assetContract.address,
+              underlying,
+              minimumSupply: BigNumber.from(10).pow(10),
+              cap: parseEther("1000"),
+            },
+          ];
+          try {
+            await vault.initialize(...args);
+          } catch (e) {
+            expect(e).to.be.an("error");
+          }
+        });
+      });
+
+      describe("commitAndClose", () => {
+        it("commits and close prior position", async () => {
+          await vault.commitAndClose();
         });
       });
     });
