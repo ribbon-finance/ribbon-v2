8a6d19f8a30d5ddf4d8f4dd3597f1d342c22702a
Ken Chan <guanhao3797@gmail.com>
2021-08-25 15:57:42 +0200

    Subtract pending from management (#73)
    * subtract from management
    
    * subtract pending amount from yearn management fee
    
    Co-authored-by: John Doe <j@j>

--- contracts/libraries/VaultLifecycle.sol
+++ contracts/libraries/VaultLifecycle.sol
@@ -431,9 +431,10 @@ library VaultLifecycle {
                 .sub(prevLockedAmount)
                 .mul(performanceFeePercent)
                 .div(100 * 10**6);
-            managementFee = currentLockedBalance.mul(managementFeePercent).div(
-                100 * 10**6
-            );
+            managementFee = currentLockedBalance
+                .sub(totalPending)
+                .mul(managementFeePercent)
+                .div(100 * 10**6);
 
             vaultFee = performanceFee.add(managementFee);
         }
