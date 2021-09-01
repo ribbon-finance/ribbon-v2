59243cfbae2af5c31d9960ac44a227f72e9ce262
kenchangh <guanhao3797@gmail.com>
2021-09-01 17:13:01 +0200

    Remove unnecessary casting

--- contracts/vaults/RibbonDeltaVault.sol
+++ contracts/vaults/RibbonDeltaVault.sol
@@ -373,7 +373,7 @@ contract RibbonDeltaVault is RibbonVault, RibbonDeltaVaultStorage {
             // Subtraction underflow checks already ensure it is smaller than uint104
             depositReceipt.amount = uint104(
                 ShareMath.sharesToUnderlying(
-                    uint256(receiptShares).sub(sharesWithdrawn),
+                    receiptShares.sub(sharesWithdrawn),
                     roundPricePerShare[depositReceipt.round],
                     vaultParams.decimals
                 )
