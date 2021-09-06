dafd402d832e46426f3947913993f9171ca7b669
kenchangh <guanhao3797@gmail.com>
2021-09-06 19:58:45 +0200

    Tests

--- contracts/tests/TestVaultLifecycle.sol
+++ contracts/tests/TestVaultLifecycle.sol
@@ -11,4 +11,35 @@ contract TestVaultLifecycle {
     {
         return VaultLifecycle.getNextFriday(currentExpiry);
     }
+
+    function balanceOf(address account) public view returns (uint256) {
+        if (account == address(this)) {
+            return 1 ether;
+        }
+    }
+
+    function rollover(
+        uint256 currentSupply,
+        address asset,
+        uint8 decimals,
+        uint256 pendingAmount,
+        uint128 queuedWithdrawShares
+    )
+        external
+        view
+        returns (
+            uint256 newLockedAmount,
+            uint256 newPricePerShare,
+            uint256 mintShares
+        )
+    {
+        return
+            VaultLifecycle.rollover(
+                currentSupply,
+                asset,
+                decimals,
+                pendingAmount,
+                queuedWithdrawShares
+            );
+    }
 }
