c31db07c8002116f96ee7694b78b6abc63183de9
Chuddy <jacob4seinfeld@gmail.com>
2021-09-07 14:38:05 -0700

    explicitly declare visibility of constants (#108)

--- contracts/libraries/SupportsNonCompliantERC20.sol
+++ contracts/libraries/SupportsNonCompliantERC20.sol
@@ -4,7 +4,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 library SupportsNonCompliantERC20 {
-    address constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address private constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
     function safeApprove(
         IERC20 token,
