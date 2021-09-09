2e0c540f609fa3ff7aa9854f91da60c15fe37893
Ken Chan <guanhao3797@gmail.com>
2021-09-09 02:51:03 +0200

    Oz audit refactor (#120)
    * Refactor calculation
    
    * Tests
    
    * Remove

--- test/libraries/VaultLifecycle.ts
+++ test/libraries/VaultLifecycle.ts
@@ -1,8 +1,9 @@
 import { ethers } from "hardhat";
-import { Contract } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import moment from "moment-timezone";
 import { assert } from "../helpers/assertions";
 import * as time from "../helpers/time";
+import { parseEther } from "ethers/lib/utils";
 
 moment.tz.setDefault("UTC");
 
@@ -12,8 +13,12 @@ describe("VaultLifecycle", () => {
   let lifecycle: Contract;
 
   before(async () => {
+    const VaultLifecycle = await ethers.getContractFactory("VaultLifecycle");
+    const lifecycleLib = await VaultLifecycle.deploy();
+
     const TestVaultLifecycle = await ethers.getContractFactory(
-      "TestVaultLifecycle"
+      "TestVaultLifecycle",
+      { libraries: { VaultLifecycle: lifecycleLib.address } }
     );
     lifecycle = await TestVaultLifecycle.deploy();
   });
@@ -127,6 +132,35 @@ describe("VaultLifecycle", () => {
       const fridayDate = moment.unix(nextFriday);
       assert.equal(fridayDate.weekday(), 5);
       assert.isTrue(fridayDate.isSame(expectedFriday));
+    });
+  });
+
+  describe("rollover", () => {
+    it("rolls over with correct math", async () => {
+      const [lockedAmount, pricePerShare, mintShares] =
+        await lifecycle.rollover(
+          parseEther("1"),
+          lifecycle.address, // pass in the lifecycle contract itself as the address to mock
+          18,
+          parseEther("0.1"),
+          parseEther("0.1")
+        );
+
+      // currentBalance - lockedAmount = queuedWithdrawAmount
+      const queuedWithdrawAmount = parseEther("1").sub(lockedAmount);
+
+      const singleShare = BigNumber.from(10).pow(18);
+
+      // (1 ether - 0.1 ether)/1 ether
+      assert.bnEqual(pricePerShare, parseEther("0.9"));
+      assert.bnEqual(
+        mintShares,
+        parseEther("0.1").mul(singleShare).div(pricePerShare)
+      );
+      assert.bnEqual(
+        queuedWithdrawAmount,
+        parseEther("0.1").mul(pricePerShare).div(singleShare)
+      );
     });
   });
 });
