aed4ea5cf08279fc653fb8ebd1006cfb76ab95ac
Ken Chan <guanhao3797@gmail.com>
2021-09-09 02:18:18 +0200

    [L06] Lack of input validation (#92)
    * Add FEE_DECIMALS
    
    * Refactor to Vault.sol
    
    * DELTA_DECIMALS
    
    * Fix test
    
    * Add input validation
    
    * Use variable
    
    * Fix test
    
    * Use DELTA_DECIMALS
    
    * Fixed managementFee
    
    * Fix lint
    
    * Fix compile

--- contracts/vaults/base/RibbonVault.sol
+++ contracts/vaults/base/RibbonVault.sol
@@ -182,6 +182,7 @@ contract RibbonVault is
             _owner,
             _feeRecipient,
             _performanceFee,
+            _managementFee,
             _tokenName,
             _tokenSymbol,
             _vaultParams
@@ -217,6 +218,7 @@ contract RibbonVault is
      */
     function setFeeRecipient(address newFeeRecipient) external onlyOwner {
         require(newFeeRecipient != address(0), "!newFeeRecipient");
+        require(newFeeRecipient != feeRecipient, "Must be new feeRecipient");
         feeRecipient = newFeeRecipient;
     }
 
