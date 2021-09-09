25fbfa31e9d2ebec608777c8f935a4f90417f2dd
kenchangh <guanhao3797@gmail.com>
2021-09-09 02:39:44 +0200

    Fix compile

--- contracts/vaults/RibbonDeltaVault.sol
+++ contracts/vaults/RibbonDeltaVault.sol
@@ -3,8 +3,10 @@ pragma solidity =0.8.4;
 
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import {Math} from "@openzeppelin/contracts/math/Math.sol";
+import {
+    SafeERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import {GnosisAuction} from "../libraries/GnosisAuction.sol";
 import {RibbonDeltaVaultStorage} from "../storage/RibbonDeltaVaultStorage.sol";
--- contracts/vaults/base/RibbonVault.sol
+++ contracts/vaults/base/RibbonVault.sol
@@ -3,10 +3,12 @@ pragma solidity =0.8.4;
 
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {
+    SafeERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {
     ReentrancyGuardUpgradeable
-} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import {
     OwnableUpgradeable
 } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
