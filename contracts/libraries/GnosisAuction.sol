45475497eb83208aede6c5eda45e2464ec74c678
kenchangh <guanhao3797@gmail.com>
2021-09-09 02:39:44 +0200

    Fix compile

--- contracts/libraries/GnosisAuction.sol
+++ contracts/libraries/GnosisAuction.sol
@@ -3,7 +3,9 @@ pragma solidity =0.8.4;
 
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {
+    SafeERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {DSMath} from "../vendor/DSMathLib.sol";
 import {IGnosisAuction} from "../interfaces/IGnosisAuction.sol";
 import {IOtoken} from "../interfaces/GammaInterface.sol";
