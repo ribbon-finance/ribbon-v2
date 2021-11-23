b25124a8af5d2dc2edfbb5a5aaf29e7c95659291
kenchangh <guanhao3797@gmail.com>
2021-11-23 23:27:16 +0800

    Rewrite unwrapYieldToken function

--- contracts/libraries/VaultLifecycleSTETH.sol
+++ contracts/libraries/VaultLifecycleSTETH.sol
@@ -332,7 +332,7 @@ library VaultLifecycleSTETH {
     }
 
     /**
-     * @notice Unwraps the necessary amount of the yield-bearing yearn token
+     * @notice Unwraps the necessary amount of the wstETH token
      *         and transfers amount to vault
      * @param amount is the amount of `asset` to withdraw
      * @param collateralToken is the address of the collateral token
@@ -353,66 +353,76 @@ library VaultLifecycleSTETH {
             "Amount withdrawn smaller than minETHOut from swap"
         );
 
-        uint256 assetBalance = address(this).balance;
+        uint256 ethAvailable = address(this).balance;
+        IERC20 steth = IERC20(stethToken);
+        uint256 stethAvailable = steth.balanceOf(address(this));
 
-        uint256 amountETHOut = DSMath.min(assetBalance, amount);
+        // 3 different success scenarios
+        // Scenario 1. We hold enough ETH to satisfy withdrawal. Send it out directly
+        // Scenario 2. We hold enough ETH + stETH to satisfy withdrawal. Do a swap
+        // Scenario 3. We hold enough wstETH to satisy withdrawal. Unwrap then swap
 
-        // We pass in the amount of stETH we want to unwrap from wstETH
-        // Though stETH != ETH, we assume that they are equivalent here
-        // by passing in the amount of ETH we need to withdraw
-        // This assumption is fine because we will be swapping the stETH to ETH.
-        uint256 stethNeeded =
-            DSMath.max(assetBalance, amount).sub(assetBalance);
-
-        uint256 wstETHPerStETH = IWSTETH(collateralToken).tokensPerStEth();
-        uint256 amountToUnwrap = stethNeeded.mul(wstETHPerStETH).div(10**18);
-
-        if (amountToUnwrap > 0) {
-            IWSTETH wsteth = IWSTETH(collateralToken);
-            IERC20 steth = IERC20(stethToken);
-
-            uint256 startStethBalance = steth.balanceOf(address(this));
-
-            if (stethNeeded > startStethBalance) {
-                amountToUnwrap = stethNeeded
-                    .sub(startStethBalance)
-                    .mul(wstETHPerStETH)
-                    .div(10**18);
-                // Unwrap to stETH
-                wsteth.unwrap(amountToUnwrap);
-            }
-
-            // Post-unwrap, the stETH balance will not completely match the stethNeeded
-            // due to precision issues.
-            // E.g. 0.5 ETH is 499999999999999998 instead of 500000000000000000
-            // We just send the entire stETH balance for the swap
-            uint256 stETHAmount =
-                steth.balanceOf(address(this)).sub(startStethBalance);
-
-            // approve steth exchange
-            steth.safeApprove(crvPool, stETHAmount);
-
-            // CRV SWAP HERE from steth -> eth
-            // 0 = ETH, 1 = STETH
-            // We are setting 1, which is the smallest possible value for the _minAmountOut parameter
-            // However it is fine because we check that the amountETHOut >= minETHOut at the end
-            // which makes sandwich attacks not possible
-            uint256 swappedAmount =
-                ICRV(crvPool).exchange(1, 0, stETHAmount, 1);
-
-            amountETHOut = amountETHOut.add(swappedAmount);
+        // Scenario 1
+        if (ethAvailable >= amount) {
+            return amount;
         }
+
+        // We unwrap if necessary first, then do the swap
+        uint256 ethstEthSum = ethAvailable.add(stethAvailable);
+        bool hasEnoughETHForWithdrawal = ethstEthSum >= amount.sub(1);
+
+        // Scenario 3
+        {
+            if (!hasEnoughETHForWithdrawal) {
+                uint256 stethNeededFromUnwrap = amount.sub(ethstEthSum);
+                uint256 wstAmountToUnwrap =
+                    IWSTETH(collateralToken).getWstETHByStETH(
+                        stethNeededFromUnwrap
+                    );
+
+                // We unwrap an additional wei because the unwrap process is off by 1
+                IWSTETH(collateralToken).unwrap(wstAmountToUnwrap);
+
+                // We do a final double check that we have enough to satisfy the amount
+                // We account for the off by 1 error
+                stethAvailable = steth.balanceOf(address(this));
+                require(
+                    ethAvailable.add(stethAvailable) >= amount.sub(2),
+                    "Unwrapping wstETH did not result in sufficient stETH"
+                );
+            }
+        }
+
+        // Scenario 2
+        // Now that we satisfied the ETH + stETH sum, we swap the stETH amounts necessary
+        // to facilitate a withdrawal
+
+        // This won't underflow since we already asserted that ethAvailable < amount before this
+        uint256 stEthAmountToSwap =
+            DSMath.min(amount.sub(ethAvailable), stethAvailable);
+
+        steth.safeApprove(crvPool, stEthAmountToSwap);
+
+        // CRV SWAP HERE from steth -> eth
+        // 0 = ETH, 1 = STETH
+        // We are setting 1, which is the smallest possible value for the _minAmountOut parameter
+        // However it is fine because we check that the amountETHOut >= minETHOut at the end
+        // which makes sandwich attacks not possible
+        uint256 ethAmountOutFromSwap =
+            ICRV(crvPool).exchange(1, 0, stEthAmountToSwap, 1);
+
+        uint256 totalETHOut = ethAvailable.add(ethAmountOutFromSwap);
 
         // This revert does not account for the ETH that is already unwrapped
         // Since minETHOut is derived from calling the Curve pool's getter,
         // it reverts in the worst case where the user needs to unwrap and sell
         // 100% of their ETH withdrawal amount
         require(
-            amountETHOut >= minETHOut,
+            totalETHOut >= minETHOut,
             "Output ETH amount smaller than minETHOut"
         );
 
-        return amountETHOut;
+        return totalETHOut;
     }
 
     /**
