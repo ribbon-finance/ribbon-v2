d0a938fded30d6c816fd2dd93aa4fa747fb25ca6
kenchangh <guanhao3797@gmail.com>
2021-12-07 16:23:31 +0800

    rename

--- scripts/deploy/10_yvusdc_eth_put_theta_vault.ts
+++ scripts/deploy/10_yvusdc_eth_put_theta_vault.ts
@@ -17,7 +17,7 @@ const main = async ({
   const { deploy } = deployments;
   const { deployer, owner, keeper, admin, feeRecipient } =
     await getNamedAccounts();
-  console.log(`09 - Deploying yvUSDC ETH Put Theta Vault on ${network.name}`);
+  console.log(`10 - Deploying yvUSDC ETH Put Theta Vault on ${network.name}`);
 
   const chainId = network.config.chainId;
 
