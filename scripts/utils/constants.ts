b8bdc61409875bfaed39966f25aec7decf37d1b8
Ben Burns <abwburns@gmail.com>
2022-01-12 17:59:59 -0500

    feat/aurora Ribbon theta vault deployment to Aurora L2 on Near

--- scripts/utils/constants.ts
+++ scripts/utils/constants.ts
@@ -14,6 +14,9 @@ export const AVAX_STRIKE_STEP = 10;
 export const ETH_STRIKE_STEP = 100; // ETH strike prices move in increments of 1000
 export const WBTC_STRIKE_STEP = 1000; // WBTC strike prices move in increments of 1000
 export const AAVE_STRIKE_STEP = 10;
+export const NEAR_STRIKE_STEP = 5;
+export const AURORA_STRIKE_STEP = 5;
+
 export const STRIKE_DELTA = 1000; // 0.1d
 export const PREMIUM_DISCOUNT = 200; // 0.20, 80% discount
 export const AUCTION_DURATION = 3600; // 1 hour
