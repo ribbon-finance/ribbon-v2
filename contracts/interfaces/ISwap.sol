02f2fb88bebd408fc0efdccc7ae1581e505ee642
Steven <steven@ribbon.finance>
2022-02-15 18:28:27 +0700

    Refined swap contract

--- contracts/interfaces/ISwap.sol
+++ contracts/interfaces/ISwap.sol
@@ -3,59 +3,69 @@
 pragma solidity ^0.8.0;
 
 interface ISwap {
+    struct Offer {
+        address seller;
+        address offeredToken;
+        address biddingToken;
+        bool isOpen;
+        uint128 minPrice;
+        uint128 minBidSize;
+        uint128 totalSize;
+        uint128 availableSize;
+    }
 
-  struct Offer {
-    address seller;
-    address offeredToken;
-    address biddingToken;
-    uint256 minPrice;
-    uint256 minBidSize;
-    uint256 totalSize;
-    uint256 availableSize;
-  }
+    struct Bid {
+        uint256 swapId;
+        uint256 nonce;
+        address signerWallet;
+        uint256 sellAmount;
+        uint256 buyAmount;
+        address referrer;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
 
-  struct Bid {
-    uint256 swapId;
-    uint256 nonce;
-    address signerWallet;
-    uint256 sellAmount;
-    uint256 buyAmount;
-    address referrer;
-    uint8 v;
-    bytes32 r;
-    bytes32 s;
-  }
+    event Swap(
+        uint256 indexed swapId,
+        uint256 nonce,
+        address indexed signerWallet,
+        uint256 signerAmount,
+        address indexed senderWallet,
+        uint256 senderAmount,
+        address referrer,
+        uint256 feeAmount
+    );
 
-  event Swap(
-    uint256 indexed nonce,
-    uint256 timestamp,
-    address indexed signerWallet,
-    address signerToken,
-    uint256 signerAmount,
-    address indexed senderWallet,
-    address senderToken,
-    uint256 senderAmount
-  );
+    event NewOffer(
+        uint256 swapId,
+        address seller,
+        address offeredToken,
+        address biddingToken,
+        uint256 minPrice,
+        uint256 minBidSize,
+        uint256 totalSize
+    );
 
-  event NewSwapOffer(
-    uint256 swapId,
-    address seller,
-    address offeredToken,
-    address biddingToken,
-    uint256 minPrice,
-    uint256 minBidSize,
-    uint256 totalSize
-  );
+    event CloseOffer(uint256 swapId);
 
-  event Cancel(uint256 indexed nonce, address indexed signerWallet);
+    event SettleOffer(uint256 swapId);
 
-  event Authorize(address indexed signer, address indexed signerWallet);
+    event Cancel(uint256 indexed nonce, address indexed signerWallet);
 
-  event Revoke(address indexed signer, address indexed signerWallet);
+    function createOffer(
+        address offeredToken,
+        address biddingToken,
+        uint128 minPrice,
+        uint128 minBidSize,
+        uint128 totalSize
+    ) external returns (uint256 swapId);
 
-  function cancel(uint256[] calldata nonces) external;
+    function settleOffer(uint256 swapId, Bid[] calldata bids) external;
 
-  function nonceUsed(address, uint256) external view returns (bool);
+    function closeOffer(uint256 swapId) external;
 
-  function authorized(address) external view returns (address);
+    function cancelNonce(uint256[] calldata nonces) external;
+
+    function nonceUsed(address, uint256) external view returns (bool);
 }
