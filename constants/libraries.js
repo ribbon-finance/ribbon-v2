8bb90014d2455d5a729daa5426925c4a7b4fe633
0xTomoyo <0xtomoyo@protonmail.com>
2022-02-22 17:48:06 +1100

    feat/Add stake() to RibbonThetaVault to allow staking Vault shares in 1 tx (#194)
    * added stake() to RibbonThetaVault
    
    * Tests for stake() function
    
    * Linter
    
    * Stake already redeemed shares
    
    * Switch from shareBalances to balanceOf
    
    * caching stakingRewards in stake()
    
    * remove nonReentrant from setters
    
    * switch from stakingRewards to liquidityGauge
    
    * added stake() to RibbonThetaVault
    
    * Tests for stake() function
    
    * Linter
    
    * Stake already redeemed shares
    
    * Switch from shareBalances to balanceOf
    
    * caching stakingRewards in stake()
    
    * remove nonReentrant from setters
    
    * switch from stakingRewards to liquidityGauge
    
    * update FORK_BLOCK
    
    * deployment
    
    * fix failing test

--- constants/libraries.js
+++ constants/libraries.js
@@ -1,4 +1,4 @@
 module.exports = {
-  VaultLifecycle: "0xe64524Ba73b9E74956f59F137821039254994f36",
+  VaultLifecycle: "0x883391710EF00F129Ed34d0118249ABfb5c73531",
   VaultLifecycleSTETH: "0x5CbB58FBd5E53A8db156C1261403b14dc618F3db",
 };
