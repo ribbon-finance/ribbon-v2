5cd1e5b62f44905a693b0af0295fca31640a3e3a
0xTomoyo <0xtomoyo@protonmail.com>
2022-02-23 00:44:35 +1100

    test steth withdrawals

--- test/upgrades/RibbonThetaSTETHVault.ts
+++ test/upgrades/RibbonThetaSTETHVault.ts
@@ -1,5 +1,6 @@
 import { assert } from "chai";
 import { ethers, network } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
 import {
   GAMMA_CONTROLLER,
   GNOSIS_EASY_AUCTION,
@@ -10,8 +11,18 @@ import {
   WSTETH_ADDRESS,
   LDO_ADDRESS,
   STETH_ETH_CRV_POOL,
+  CHAINLINK_WETH_PRICER_STETH,
+  WSTETH_PRICER,
+  YEARN_PRICER_OWNER,
 } from "../../constants/constants";
-import { objectEquals, parseLog, serializeMap } from "../helpers/utils";
+import {
+  objectEquals,
+  parseLog,
+  serializeMap,
+  setOpynOracleExpiryPriceYearn,
+  setupOracle,
+  getAssetPricer,
+} from "../helpers/utils";
 import deployments from "../../constants/deployments.json";
 import { BigNumberish, Contract } from "ethers";
 import * as time from "../helpers/time";
@@ -25,50 +36,17 @@ const IMPLEMENTATION_SLOT =
 // UPDATE THESE VALUES BEFORE WE ATTEMPT AN UPGRADE
 const FORK_BLOCK = 14254363;
 
+const TEST_USER = "0x79F1bc5c5ee691206Fc55C66402F938dd1E96421";
+
 const CHAINID = process.env.CHAINID ? Number(process.env.CHAINID) : 1;
 
 describe("RibbonThetaSTETHVault upgrade", () => {
-  let vaults: string[] = [];
-
-  before(async function () {
-    // We need to checkpoint the contract on mainnet to a past block before the upgrade happens
-    // This means the `implementation` is pointing to an old contract
-    await network.provider.request({
-      method: "hardhat_reset",
-      params: [
-        {
-          forking: {
-            jsonRpcUrl: process.env.TEST_URI,
-            blockNumber: FORK_BLOCK,
-          },
-        },
-      ],
-    });
-
-    // Fund & impersonate the admin account
-    const [userSigner] = await ethers.getSigners();
-
-    await userSigner.sendTransaction({
-      to: UPGRADE_ADMIN,
-      value: parseEther("10"),
-    });
-
-    await network.provider.request({
-      method: "hardhat_impersonateAccount",
-      params: [UPGRADE_ADMIN],
-    });
-
-    const deploymentNames = ["RibbonThetaVaultSTETHCall"];
-    deploymentNames.forEach((name) => vaults.push(deployments.mainnet[name]));
-  });
-
-  checkIfStorageNotCorrupted(deployments.mainnet.RibbonThetaVaultSTETHCall);
-});
-
-function checkIfStorageNotCorrupted(vaultAddress: string) {
-  const getVaultStorage = async (storageIndex: BigNumberish) => {
-    return await ethers.provider.getStorageAt(vaultAddress, storageIndex);
-  };
+  let newImplementation: string;
+  let vaultProxy: Contract;
+  let vault: Contract;
+  let keeper: string;
+  let ownerSigner: SignerWithAddress;
+  let variables: Record<string, unknown> = {};
 
   const variableNames = [
     "vaultParams",
@@ -91,57 +69,108 @@ function checkIfStorageNotCorrupted(vaultAddress: string) {
     "lastQueuedWithdrawAmount",
   ];
 
-  let variables: Record<string, unknown> = {};
-
-  describe(`Vault ${vaultAddress}`, () => {
-    let newImplementation: string;
-    let vaultProxy: Contract;
-    let vault: Contract;
-
-    time.revertToSnapshotAfterEach();
-
-    before(async () => {
-      const adminSigner = await ethers.provider.getSigner(UPGRADE_ADMIN);
-
-      vaultProxy = await ethers.getContractAt(
-        "AdminUpgradeabilityProxy",
-        vaultAddress,
-        adminSigner
-      );
-      vault = await ethers.getContractAt("RibbonThetaSTETHVault", vaultAddress);
-
-      variables = await getVariablesFromContract(vault);
-
-      const VaultLifecycle = await ethers.getContractFactory("VaultLifecycle");
-      const VaultLifecycleLib = await VaultLifecycle.deploy();
-
-      const VaultLifecycleSTETH = await ethers.getContractFactory(
-        "VaultLifecycleSTETH"
-      );
-      const VaultLifecycleSTETHLib = await VaultLifecycleSTETH.deploy();
-
-      const RibbonThetaSTETHVault = await ethers.getContractFactory(
-        "RibbonThetaSTETHVault",
+  before(async function () {
+    // We need to checkpoint the contract on mainnet to a past block before the upgrade happens
+    // This means the `implementation` is pointing to an old contract
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
         {
-          libraries: {
-            VaultLifecycle: VaultLifecycleLib.address,
-            VaultLifecycleSTETH: VaultLifecycleSTETHLib.address,
+          forking: {
+            jsonRpcUrl: process.env.TEST_URI,
+            blockNumber: FORK_BLOCK,
           },
-        }
-      );
-      const newImplementationContract = await RibbonThetaSTETHVault.deploy(
-        WETH_ADDRESS[CHAINID],
-        USDC_ADDRESS[CHAINID],
-        WSTETH_ADDRESS[CHAINID],
-        LDO_ADDRESS,
-        OTOKEN_FACTORY[CHAINID],
-        GAMMA_CONTROLLER[CHAINID],
-        MARGIN_POOL[CHAINID],
-        GNOSIS_EASY_AUCTION[CHAINID],
-        STETH_ETH_CRV_POOL
-      );
-      newImplementation = newImplementationContract.address;
+        },
+      ],
     });
+
+    // Fund & impersonate the admin account
+    [ownerSigner] = await ethers.getSigners();
+
+    await ownerSigner.sendTransaction({
+      to: UPGRADE_ADMIN,
+      value: parseEther("10"),
+    });
+
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [UPGRADE_ADMIN],
+    });
+
+    await ownerSigner.sendTransaction({
+      to: TEST_USER,
+      value: parseEther("10"),
+    });
+
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [TEST_USER],
+    });
+
+    const adminSigner = await ethers.provider.getSigner(UPGRADE_ADMIN);
+
+    vaultProxy = await ethers.getContractAt(
+      "AdminUpgradeabilityProxy",
+      deployments.mainnet.RibbonThetaVaultSTETHCall,
+      adminSigner
+    );
+    vault = await ethers.getContractAt(
+      "RibbonThetaSTETHVault",
+      deployments.mainnet.RibbonThetaVaultSTETHCall
+    );
+
+    variables = await getVariablesFromContract(vault);
+
+    const VaultLifecycle = await ethers.getContractFactory("VaultLifecycle");
+    const VaultLifecycleLib = await VaultLifecycle.deploy();
+
+    const VaultLifecycleSTETH = await ethers.getContractFactory(
+      "VaultLifecycleSTETH"
+    );
+    const VaultLifecycleSTETHLib = await VaultLifecycleSTETH.deploy();
+
+    const RibbonThetaSTETHVault = await ethers.getContractFactory(
+      "RibbonThetaSTETHVault",
+      {
+        libraries: {
+          VaultLifecycle: VaultLifecycleLib.address,
+          VaultLifecycleSTETH: VaultLifecycleSTETHLib.address,
+        },
+      }
+    );
+    const newImplementationContract = await RibbonThetaSTETHVault.deploy(
+      WETH_ADDRESS[CHAINID],
+      USDC_ADDRESS[CHAINID],
+      WSTETH_ADDRESS[CHAINID],
+      LDO_ADDRESS,
+      OTOKEN_FACTORY[CHAINID],
+      GAMMA_CONTROLLER[CHAINID],
+      MARGIN_POOL[CHAINID],
+      GNOSIS_EASY_AUCTION[CHAINID],
+      STETH_ETH_CRV_POOL
+    );
+    newImplementation = newImplementationContract.address;
+
+    keeper = await vault.keeper();
+
+    await ownerSigner.sendTransaction({
+      to: keeper,
+      value: parseEther("10"),
+    });
+
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [keeper],
+    });
+
+    await ownerSigner.sendTransaction({
+      to: YEARN_PRICER_OWNER,
+      value: parseEther("10"),
+    });
+  });
+
+  describe("Upgrade checks", () => {
+    time.revertToSnapshotAfterEach();
 
     it("has the correct return values for all public variables", async () => {
       await vaultProxy.upgradeTo(newImplementation);
@@ -166,16 +195,79 @@ New: ${JSON.stringify(newVariables, null, 4)}`
         "0x000000000000000000000000" + newImplementation.slice(2).toLowerCase()
       );
     });
-
-    const getVariablesFromContract = async (vault: Contract) => {
-      // get contract values with solidity getter
-      const variableReturns = await Promise.all(
-        variableNames.map((varName) => vault[varName]())
-      );
-      const variables = Object.fromEntries(
-        variableNames.map((varName, index) => [varName, variableReturns[index]])
-      );
-      return serializeMap(variables);
-    };
   });
-}
+
+  describe("#completeWithdraw", () => {
+    time.revertToSnapshotAfterEach();
+
+    beforeEach(async function () {
+      await vaultProxy.upgradeTo(newImplementation);
+    });
+
+    it("Withdraws stETH", async () => {
+      const userSigner = await ethers.provider.getSigner(TEST_USER);
+      const keeperSigner = await ethers.provider.getSigner(keeper);
+
+      const vaultBalance = await vault.shares(TEST_USER);
+      const initialWithdrawal = vaultBalance.div(2);
+
+      await vault.connect(userSigner).initiateWithdraw(initialWithdrawal);
+
+      const collateralPricerSigner = await getAssetPricer(
+        WSTETH_PRICER,
+        ownerSigner
+      );
+
+      const oracle = await setupOracle(
+        CHAINLINK_WETH_PRICER_STETH,
+        ownerSigner
+      );
+
+      await setOpynOracleExpiryPriceYearn(
+        WETH_ADDRESS[CHAINID],
+        oracle,
+        await getCurrentOptionStrike(),
+        collateralPricerSigner,
+        await getCurrentOptionExpiry()
+      );
+
+      await vault.connect(keeperSigner).commitAndClose();
+
+      await time.increaseTo((await vault.nextOptionReadyAt()).toNumber() + 1);
+
+      await vault.connect(keeperSigner).rollToNextOption();
+
+      await vault.connect(userSigner).completeWithdraw(0);
+    });
+  });
+
+  const getVaultStorage = async (storageIndex: BigNumberish) => {
+    return await ethers.provider.getStorageAt(
+      deployments.mainnet.RibbonThetaVaultSTETHCall,
+      storageIndex
+    );
+  };
+
+  const getVariablesFromContract = async (vault: Contract) => {
+    // get contract values with solidity getter
+    const variableReturns = await Promise.all(
+      variableNames.map((varName) => vault[varName]())
+    );
+    const variables = Object.fromEntries(
+      variableNames.map((varName, index) => [varName, variableReturns[index]])
+    );
+    return serializeMap(variables);
+  };
+
+  const getCurrentOptionExpiry = async () => {
+    const currentOption = await vault.currentOption();
+    const otoken = await ethers.getContractAt("IOtoken", currentOption);
+    return otoken.expiryTimestamp();
+  };
+
+  const getCurrentOptionStrike = async () => {
+    const currentOption = await vault.currentOption();
+    const otoken = await ethers.getContractAt("IOtoken", currentOption);
+    return otoken.strikePrice();
+  };
+});
