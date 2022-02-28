6ebcb361efb8ebcc84505b3b2fb2ef82955b1670
0xTomoyo <0xtomoyo@protonmail.com>
2022-02-28 16:54:56 +1100

    Deploy RibbonThetaSTETHVault + RibbonThetaYearnVault (#225)
    * deploy steth and yearn vaults
    
    * update fork blocks

--- constants/libraries.js
+++ constants/libraries.js
@@ -1,4 +1,4 @@
 module.exports = {
   VaultLifecycle: "0x883391710EF00F129Ed34d0118249ABfb5c73531",
-  VaultLifecycleSTETH: "0x5CbB58FBd5E53A8db156C1261403b14dc618F3db",
+  VaultLifecycleSTETH: "0xbe12A3f511b5c0fd25c6cE8688E186955C4F3D02",
 };
