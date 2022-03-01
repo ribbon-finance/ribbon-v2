931774ce2971dcc6d1d6e90afea8f7ac2845f5f5
0xTomoyo <0xtomoyo@protonmail.com>
2022-03-01 12:03:56 +1100

    init

---
+++ contracts/interfaces/PowerTokenInterface.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.4;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface IController {
+    function ethQuoteCurrencyPool() external view returns (address);
+
+    function feeRate() external view returns (uint256);
+
+    function getFee(
+        uint256 _vaultId,
+        uint256 _wPowerPerpAmount,
+        uint256 _collateralAmount
+    ) external view returns (uint256);
+
+    function quoteCurrency() external view returns (address);
+
+    // function vaults(uint256 _vaultId) external view returns (VaultLib.Vault memory);
+
+    function shortPowerPerp() external view returns (address);
+
+    function wPowerPerp() external view returns (address);
+
+    function getExpectedNormalizationFactor() external view returns (uint256);
+
+    function mintPowerPerpAmount(
+        uint256 _vaultId,
+        uint256 _powerPerpAmount,
+        uint256 _uniTokenId
+    ) external payable returns (uint256 vaultId, uint256 wPowerPerpAmount);
+
+    function mintWPowerPerpAmount(
+        uint256 _vaultId,
+        uint256 _wPowerPerpAmount,
+        uint256 _uniTokenId
+    ) external payable returns (uint256 vaultId);
+
+    /**
+     * Deposit collateral into a vault
+     */
+    function deposit(uint256 _vaultId) external payable;
+
+    /**
+     * Withdraw collateral from a vault.
+     */
+    function withdraw(uint256 _vaultId, uint256 _amount) external payable;
+
+    function burnWPowerPerpAmount(
+        uint256 _vaultId,
+        uint256 _wPowerPerpAmount,
+        uint256 _withdrawAmount
+    ) external;
+
+    function burnOnPowerPerpAmount(
+        uint256 _vaultId,
+        uint256 _powerPerpAmount,
+        uint256 _withdrawAmount
+    ) external returns (uint256 wPowerPerpAmount);
+
+    function liquidate(uint256 _vaultId, uint256 _maxDebtAmount)
+        external
+        returns (uint256);
+
+    function updateOperator(uint256 _vaultId, address _operator) external;
+
+    /**
+     * External function to update the normalized factor as a way to pay funding.
+     */
+    function applyFunding() external;
+
+    function redeemShort(uint256 _vaultId) external;
+
+    function reduceDebtShutdown(uint256 _vaultId) external;
+
+    function isShutDown() external returns (bool);
+}
+
+interface IWPowerPerp is IERC20 {
+    function mint(address _account, uint256 _amount) external;
+
+    function burn(address _account, uint256 _amount) external;
+}
+
+interface IOracle {
+    function getHistoricalTwap(
+        address _pool,
+        address _base,
+        address _quote,
+        uint32 _period,
+        uint32 _periodToHistoricPrice
+    ) external view returns (uint256);
+
+    function getTwap(
+        address _pool,
+        address _base,
+        address _quote,
+        uint32 _period,
+        bool _checkPeriod
+    ) external view returns (uint256);
+
+    function getMaxPeriod(address _pool) external view returns (uint32);
+
+    function getTimeWeightedAverageTickSafe(address _pool, uint32 _period)
+        external
+        view
+        returns (int24 timeWeightedAverageTick);
+}
+
+interface IShortPowerPerp is IERC721 {
+    function nextId() external view returns (uint256);
+
+    function mintNFT(address recipient) external returns (uint256 _newId);
+}
