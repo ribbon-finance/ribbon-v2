e763178687a6744099edeffb2f8b37cab3afe6ab
Your Name <you@example.com>
2022-06-15 16:57:32 -0700

    add reth logic deploy script

---
+++ scripts/deploy/21_theta_vault_reth_logic.ts
@@ -0,0 +1,73 @@
+import { run } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import {
+  WETH_ADDRESS,
+  USDC_ADDRESS,
+  OTOKEN_FACTORY,
+  GAMMA_CONTROLLER,
+  MARGIN_POOL,
+  GNOSIS_EASY_AUCTION,
+} from "../../constants/constants";
+
+const main = async ({
+  network,
+  deployments,
+  getNamedAccounts,
+}: HardhatRuntimeEnvironment) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  console.log(`01 - Deploying Theta Vault RETH logic on ${network.name}`);
+
+  const chainId = network.config.chainId;
+
+  const lifecycle = await deploy("VaultLifecycle", {
+    contract: "VaultLifecycle",
+    from: deployer,
+  });
+  console.log(`VaultLifeCycle @ ${lifecycle.address}`);
+
+  const vault = await deploy("RibbonThetaVaultLogic", {
+    contract: "RibbonThetaRETHVault",
+    from: deployer,
+    args: [
+      WETH_ADDRESS[chainId],
+      USDC_ADDRESS[chainId],
+      OTOKEN_FACTORY[chainId],
+      GAMMA_CONTROLLER[chainId],
+      MARGIN_POOL[chainId],
+      GNOSIS_EASY_AUCTION[chainId],
+    ],
+    libraries: {
+      VaultLifecycle: lifecycle.address,
+    },
+  });
+  console.log(`RibbonThetaVaultRETHLogic @ ${vault.address}`);
+
+  try {
+    await run("verify:verify", {
+      address: lifecycle.address,
+      constructorArguments: [],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    await run("verify:verify", {
+      address: vault.address,
+      constructorArguments: [
+        WETH_ADDRESS[chainId],
+        USDC_ADDRESS[chainId],
+        OTOKEN_FACTORY[chainId],
+        GAMMA_CONTROLLER[chainId],
+        MARGIN_POOL[chainId],
+        GNOSIS_EASY_AUCTION[chainId],
+      ],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+main.tags = ["RibbonThetaVaultRETHLogic"];
+
+export default main;
---
+++ scripts/deploy/22_reth_theta_vault.ts
@@ -0,0 +1,97 @@
+import { run } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { CHAINID, RETH_ADDRESS } from "../../constants/constants";
+import {
+  MANAGEMENT_FEE,
+  PERFORMANCE_FEE,
+  PREMIUM_DISCOUNT,
+  AUCTION_DURATION,
+} from "../utils/constants";
+
+const main = async ({
+  network,
+  deployments,
+  ethers,
+  getNamedAccounts,
+}: HardhatRuntimeEnvironment) => {
+  const chainId = network.config.chainId;
+
+  if (chainId === CHAINID.AVAX_MAINNET || chainId === CHAINID.AVAX_FUJI) {
+    console.log(
+      `22 - Skipping deployment rETH Call Theta Vault on ${network.name} because no rETH on Avax`
+    );
+    return;
+  }
+
+  const { BigNumber } = ethers;
+  const { parseEther } = ethers.utils;
+  const { deploy } = deployments;
+  const { deployer, owner, keeper, admin, feeRecipient } =
+    await getNamedAccounts();
+
+  console.log(`22 - Deploying rETH Call Theta Vault on ${network.name}`);
+
+  const pricer = await deployments.get("OptionsPremiumPricerETHCall");
+  const strikeSelection = "0xab40513b6f0a33a68b59ccf90cb6f892b4be1573"; //await deployments.get("StrikeSelectionETH");
+
+  const logicDeployment = await deployments.get("RibbonThetaVaultRETHLogic");
+  const lifecycle = await deployments.get("VaultLifecycle");
+
+  const RibbonThetaRETHVault = await ethers.getContractFactory(
+    "RibbonThetaRETHVault",
+    {
+      libraries: {
+        VaultLifecycle: lifecycle.address,
+      },
+    }
+  );
+
+  const initArgs = [
+    {
+      _owner: owner,
+      _keeper: keeper,
+      _feeRecipient: feeRecipient,
+      _managementFee: MANAGEMENT_FEE,
+      _performanceFee: PERFORMANCE_FEE,
+      _tokenName: "Ribbon rETH Theta Vault",
+      _tokenSymbol: "rrETH-THETA",
+      _optionsPremiumPricer: pricer.address,
+      _strikeSelection: strikeSelection,
+      _premiumDiscount: PREMIUM_DISCOUNT,
+      _auctionDuration: AUCTION_DURATION,
+    },
+    {
+      isPut: false,
+      decimals: 18,
+      asset: RETH_ADDRESS[chainId],
+      underlying: RETH_ADDRESS[chainId],
+      minimumSupply: BigNumber.from(10).pow(10),
+      cap: parseEther("1000"),
+    },
+  ];
+  const initData = RibbonThetaRETHVault.interface.encodeFunctionData(
+    "initialize",
+    initArgs
+  );
+
+  const proxy = await deploy("RibbonThetaVaultRETHCall", {
+    contract: "AdminUpgradeabilityProxy",
+    from: deployer,
+    args: [logicDeployment.address, admin, initData],
+  });
+
+  console.log(`RibbonThetaVaultRETHCall Proxy @ ${proxy.address}`);
+
+  try {
+    await run("verify:verify", {
+      address: proxy.address,
+      constructorArguments: [logicDeployment.address, admin, initData],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+main.tags = ["RibbonThetaVaultRETHCall"];
+main.dependencies = [];
+
+export default main;
