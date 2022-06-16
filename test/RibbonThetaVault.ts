ca719e5f120902758b5dafe70f5025d2dd8672fb
chudnov <jacob4seinfeld@gmail.com>
2022-06-15 19:34:07 -0700

    24 transfer window

--- test/RibbonThetaVault.ts
+++ test/RibbonThetaVault.ts
@@ -1242,8 +1242,8 @@ function behavesLikeRibbonOptionsVault(params: {
             .connect(adminSigner)
             .deposit({ value: parseEther("10") });
 
-          // Wait 12 hours to allow reth transfer after mint
-          await time.increase(43200000);
+          // Wait 24 hours to allow reth transfer after mint
+          await time.increase(86400000);
 
           await assetContract
             .connect(adminSigner)
