ed5cf78982fb484c517b7eb62617c6f131556f50
Your Name <you@example.com>
2022-06-24 18:51:33 +0800

    change auction duration to 10 min

--- scripts/utils/constants.ts
+++ scripts/utils/constants.ts
@@ -12,7 +12,7 @@ export const STRIKE_STEP = {
 
 export const STRIKE_DELTA = 1000; // 0.1d
 export const PREMIUM_DISCOUNT = 200; // 0.20, 80% discount
-export const AUCTION_DURATION = 3600; // 1 hour
+export const AUCTION_DURATION = 600; // 10 minutes
 export const PERFORMANCE_FEE = 10000000;
 export const MANAGEMENT_FEE = 2000000; // 2% per year. 2 * 10**6. Should result in 38356 per week.
 
