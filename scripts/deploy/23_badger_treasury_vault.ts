d9091a43d4e2f1734131efaafa1d4b4bcb8ba77e
Your Name <you@example.com>
2022-06-27 18:35:30 +0800

    use optionId

--- scripts/deploy/23_badger_treasury_vault.ts
+++ scripts/deploy/23_badger_treasury_vault.ts
@@ -37,9 +37,20 @@ const main = async ({
     return;
   }
 
-  const manualVolOracle = "0x7c0bfab993fbd54deadf43adff960e9c9e116399"; //await deployments.get("ManualVolOracle");
+  const manualVolOracle = await deployments.get("ManualVolOracle");
   const underlyingOracle = BADGER_PRICE_ORACLE[chainId];
   const stablesOracle = USDC_PRICE_ORACLE[chainId];
+
+  const manualVolOracleContract = await ethers.getContractAt(
+    ManualVolOracle_ABI,
+    manualVolOracle.address
+  );
+  const optionId = await manualVolOracleContract.getOptionId(
+    getDeltaStep("BADGER"),
+    BADGER_ADDRESS[chainId],
+    BADGER_ADDRESS[chainId],
+    false
+  );
 
   const pricer = await deploy("OptionsPremiumPricerBADGER", {
     from: deployer,
@@ -47,12 +58,7 @@ const main = async ({
       abi: OptionsPremiumPricerInStables_ABI,
       bytecode: OptionsPremiumPricerInStables_BYTECODE,
     },
-    args: [
-      BADGER_ETH_POOL[chainId],
-      manualVolOracle.address,
-      underlyingOracle,
-      stablesOracle,
-    ],
+    args: [optionId, manualVolOracle.address, underlyingOracle, stablesOracle],
   });
 
   console.log(`RibbonTreasuryVaultBADGER pricer @ ${pricer.address}`);
@@ -143,6 +149,6 @@ const main = async ({
   }
 };
 main.tags = ["RibbonTreasuryVaultBADGER"];
-main.dependencies = [];//["ManualVolOracle", "RibbonTreasuryVaultLogic"];
+main.dependencies = []; //["ManualVolOracle", "RibbonTreasuryVaultLogic"];
 
 export default main;
