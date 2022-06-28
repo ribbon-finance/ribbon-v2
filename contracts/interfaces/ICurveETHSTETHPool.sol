d4745354b0507c9a1581e965dd58de91ae7614f2
Chan Jing Hong <jinghongchan@gmail.com>
2022-06-28 15:58:54 +0800

    WIP: Added STETHDepositHelper + tests

---
+++ contracts/interfaces/ICurveETHSTETHPool.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface ICurveETHSTETHPool {
+    function get_dy(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external payable returns (uint256);
+}
