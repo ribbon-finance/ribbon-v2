7de1c8ecf25dd2b91a8cc87ea6b814a25991a9c5
Steven <steven@ribbon.finance>
2022-07-07 12:13:57 +0800

    style: format

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -256,7 +256,9 @@ export async function setupOracle(
         .connect(oracleOwnerSigner)
         .setAssetPricer(collateralAssetAddr, chainlinkPricer);
     } else {
-      if (collateralAssetAddr !== "0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE") {
+      if (
+        collateralAssetAddr !== "0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE"
+      ) {
         await oracle
           .connect(oracleOwnerSigner)
           .setAssetPricer(assetAddr, chainlinkPricer);
@@ -272,7 +274,9 @@ export async function setupOracle(
         .connect(oracleOwnerSigner)
         .updateAssetPricer(collateralAssetAddr, chainlinkPricer);
     } else {
-      if (collateralAssetAddr !== "0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE") {
+      if (
+        collateralAssetAddr !== "0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE"
+      ) {
         await oracle
           .connect(oracleOwnerSigner)
           .updateAssetPricer(assetAddr, chainlinkPricer);
@@ -323,12 +327,14 @@ export async function setOpynOracleExpiryPrice(
       const pricerAddress = await oracle.getPricer(asset);
       await getAssetPricer(pricerAddress, oracle.signer as SignerWithAddress);
       const pricerSigner = provider.getSigner(pricerAddress);
-      const res = await oracle.connect(pricerSigner).setExpiryPrice(asset, expiry, settlePrice);
+      const res = await oracle
+        .connect(pricerSigner)
+        .setExpiryPrice(asset, expiry, settlePrice);
       receipt = await res.wait();
-   } else {
+    } else {
       const res = await oracle.setExpiryPrice(asset, expiry, settlePrice);
       receipt = await res.wait();
-   }
+    }
   }
   const timestamp = (await provider.getBlock(receipt.blockNumber)).timestamp;
 
