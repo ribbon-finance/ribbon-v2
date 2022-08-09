3ef5fbc4cb7904ecc9e17a9e65445612fd139824
kenchangh <guanhao3797@gmail.com>
2022-08-09 16:29:07 +0800

    remove test case

--- test/RibbonThetaVault.ts
+++ test/RibbonThetaVault.ts
@@ -236,40 +236,6 @@ describe("RibbonThetaVault", () => {
     protocol: OPTION_PROTOCOL.TD,
     contractType: "RibbonThetaVault",
   });
-
-  behavesLikeRibbonOptionsVault({
-    name: `Ribbon RETH Theta Vault (Call)`,
-    tokenName: "Ribbon RETH Theta Vault",
-    tokenSymbol: "rRETH-THETA",
-    asset: WETH_ADDRESS[chainId],
-    assetContractName: "IWETH",
-    strikeAsset: USDC_ADDRESS[chainId],
-    collateralAsset: RETH_ADDRESS[chainId],
-    chainlinkPricer: RETH_PRICER,
-    deltaFirstOption: BigNumber.from("1000"),
-    deltaSecondOption: BigNumber.from("1000"),
-    deltaStep: getDeltaStep("WETH"),
-    depositAmount: parseEther("1"),
-    minimumSupply: BigNumber.from("10").pow("10").toString(),
-    expectedMintAmount: BigNumber.from("100000000"),
-    premiumDiscount: BigNumber.from("997"),
-    managementFee: BigNumber.from("2000000"),
-    performanceFee: BigNumber.from("20000000"),
-    auctionDuration: 21600,
-    tokenDecimals: 18,
-    isPut: false,
-    gasLimits: {
-      depositWorstCase: 109576,
-      depositBestCase: 93300,
-    },
-    mintConfig: {
-      amount: parseEther("1000"),
-      contractOwnerAddress: RETH_OWNER_ADDRESS[chainId],
-    },
-    availableChains: [CHAINID.ETH_MAINNET],
-    protocol: OPTION_PROTOCOL.GAMMA,
-    contractType: "RibbonThetaRETHVault",
-  });
 });
 
 type Option = {
