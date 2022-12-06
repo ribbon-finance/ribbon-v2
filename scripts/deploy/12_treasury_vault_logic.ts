cd144bb1f328e862b485bf180944ac5fedbea229
Sahilgat <nisgat@live.com>
2022-12-07 02:26:58 +0800

    fix bare issue

--- scripts/deploy/12_treasury_vault_logic.ts
+++ scripts/deploy/12_treasury_vault_logic.ts
@@ -49,7 +49,7 @@ const main = async ({
       "0x0b7fFc1f4AD541A4Ed16b40D8c37f0929158D101",
     ],
     libraries: {
-      VaultLifecycleTreasury: lifecycleTreasury.address,
+      VaultLifecycleTreasuryBare: lifecycleTreasury.address,
     },
   });
 
