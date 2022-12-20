d844ed36bbebd5c691bb554a5825869aaf8ec436
Sahilgat <nisgat@live.com>
2022-12-20 14:18:29 +0800

    Remove setPremiumDIscount

--- contracts/vaults/TreasuryVault/RibbonTreasuryVaultWithAirSwap.sol
+++ contracts/vaults/TreasuryVault/RibbonTreasuryVaultWithAirSwap.sol
@@ -299,22 +299,6 @@ contract RibbonTreasuryVaultWithAirSwap is
     }
 
     /**
-     * @notice Sets the new discount on premiums for options we are selling
-     * @param newPremiumDiscount is the premium discount
-     */
-    function setPremiumDiscount(uint256 newPremiumDiscount) external onlyOwner {
-        require(
-            newPremiumDiscount > 0 &&
-                newPremiumDiscount < 100 * Vault.PREMIUM_DISCOUNT_MULTIPLIER,
-            "Invalid discount"
-        );
-
-        emit PremiumDiscountSet(premiumDiscount, newPremiumDiscount);
-
-        premiumDiscount = newPremiumDiscount;
-    }
-
-    /**
      * @notice Sets the new strike selection contract
      * @param newStrikeSelection is the address of the new strike selection contract
      */
